9e50ea777e3a29e38527c54aa5bf4e6d2a71569d
casper1051 <90978578+casper1051@users.noreply.github.com>
2025-07-13 17:20:23 -0700

    Implement

--- install.sh
+++ install.sh
@@ -1,1 +1,28 @@
 #!/bin/bash
+
+#So the way to launch this UI and not conflict with BotUI is to simply reroute the wombat_launcher.sh start script
+#First we should copy the original script so we can revert back if we want to uninstall
+
+echo "Setting up..."
+cp /home/kipr/
+cp wombat_launcher.sh wombat_launcher_original.sh
+
+#Now we can write the new launch file. ui.py and start_wifi.sh will be started later
+echo "Re-writing /home/kipr/wombat_launcher.sh... Find the original at /home/kipr/wombat_launcher_original.sh"
+echo -e "#!/bin/bash \npython3 /home/kipr/ui.py & \n/home/kipr/start_wifi.sh &" > wombat_launcher.sh
+
+
+#Now we make the ui.py file
+echo "Fetching main ui file..."
+wget https://raw.githubusercontent.com/casper1051/novaui/refs/heads/main/main.py
+
+#Move to the right file place
+echo "Moving..."
+mv main.py ui.py
+
+#Now for make_wifi.sh
+echo "Fetching wifi AP creation script..."
+#TODO: Make client mode
+wget https://raw.githubusercontent.com/casper1051/novaui/refs/heads/main/start_wifi.sh
+
+echo "Finished"
