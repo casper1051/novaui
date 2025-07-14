b2f480d0af26a2fd5090e50f137b1ffe0a8044ec
casper1051 <90978578+casper1051@users.noreply.github.com>
2025-07-13 18:25:27 -0700

    Fix permissions and wget for ui file

--- install.sh
+++ install.sh
@@ -10,17 +10,13 @@ cp wombat_launcher.sh wombat_launcher_original.sh
 #Now we can write the new launch file. ui.py and start_wifi.sh will be started later
 echo "Re-writing /home/kipr/wombat_launcher.sh... Find the original at /home/kipr/wombat_launcher_original.sh"
 echo -e "#!/bin/bash \nsudo python3 /home/kipr/ui.py & \n/home/kipr/start_wifi.sh &" > wombat_launcher.sh
-chmod +x wombat_launcher.sh
+sudo chmod +x wombat_launcher.sh
 
 
 #Now we make the ui.py file
 echo "Fetching main ui file..."
-wget -q -O https://raw.githubusercontent.com/casper1051/novaui/refs/heads/main/main.py
-
-#Move to the right file place
-echo "Moving..."
 rm ui.py #Reinstall to update? Remove files already here
-mv main.py ui.py
+wget -q -O ui.py https://raw.githubusercontent.com/casper1051/novaui/refs/heads/main/main.py
 
 #Now for make_wifi.sh
 echo "Fetching wifi AP creation script..."
@@ -28,7 +24,7 @@ echo "Fetching wifi AP creation script..."
 #TODO: Make client mode
 rm start_wifi.sh
 wget -q -O start_wifi.sh https://raw.githubusercontent.com/casper1051/novaui/refs/heads/main/start_wifi.sh
-chmod +x start_wifi.sh
+sudo chmod +x start_wifi.sh
 
 
 echo "Finished"
